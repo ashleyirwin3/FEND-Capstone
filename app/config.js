91e740fdc9f375c891ab731617743887c9b24dab
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-02-06 15:48:38 -0600

    edited routes

--- app/config.js
+++ app/config.js
@@ -2,28 +2,28 @@ app.config(function ($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
   $routeProvider
   .when('/', {
-  controller: 'HomeCtrl',
-  templateUrl: '/partials/home.html'
+    controller: 'HomeCtrl',
+    templateUrl: '/partials/home.html',
   })
-  .when('/', {
-  controller: 'BrowseCtrl',
-  templateUrl: '/partials/browse.html'
+  .when('/browse', {
+    controller: 'BrowseCtrl',
+    templateUrl: '/partials/browse.html',
   })
-  .when('/', {
-  controller: 'GroupsCtrl',
-  templateUrl: '/partials/groups.html'
+  .when('/groups', {
+    controller: 'GroupsCtrl',
+    templateUrl: '/partials/groups.html',
   })
-  .when('/', {
-  controller: 'LoginCtrl',
-  templateUrl: '/partials/login.html'
+  .when('/login', {
+    controller: 'LoginCtrl',
+    templateUrl: '/partials/login.html',
   })
-  .when('/', {
-  controller: 'ProfileCtrl',
-  templateUrl: '/partials/profile.html'
+  .when('/profile', {
+    controller: 'ProfileCtrl',
+    templateUrl: '/partials/profile.html',
   })
-  .when('/', {
-  controller: 'RegisterCtrl',
-  templateUrl: '/partials/register.html'
+  .when('/register', {
+    controller: 'RegisterCtrl',
+    templateUrl: '/partials/register.html',
   })
   .otherwise({
     redirectTo: ('/')
