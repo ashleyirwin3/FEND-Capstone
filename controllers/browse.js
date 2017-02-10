c53f69c2b53d39c6ceebd9248fc998bfd0e90157
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-02-10 10:31:55 -0600

    made httprequest for api json

--- controllers/browse.js
+++ controllers/browse.js
@@ -1,5 +1,10 @@
 console.log('browse controller')
 
-app.controller('BrowseCtrl', function() {
 
-})
+
+app.controller('BrowseCtrl', function($http, $scope) {
+  $http.get(`https://www.googleapis.com/books/v1/volumes?q=holes+inauthor:sachar&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
+  .then(function(response) {
+        $scope.getBooks = response.data
+    })
+  })
