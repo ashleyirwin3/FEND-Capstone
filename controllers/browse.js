3d39117f7c9b47ae18a38bf5327a57aa59034ef1
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-02-10 14:52:30 -0600

    added scopes for book info to load onto dom

--- controllers/browse.js
+++ controllers/browse.js
@@ -3,9 +3,16 @@ console.log('browse controller')
 
 
 app.controller('BrowseCtrl', function($http, $scope) {
-  $http.get(`https://www.googleapis.com/books/v1/volumes?q=flower+inauthor:keyes&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
-  .then(function(response) {
-        $scope.getBooks = response.data
-
-    })
+  $scope.getBooks = ()=> {
+        console.log('hey')
+    $http.get(`https://www.googleapis.com/books/v1/volumes?q=night+inauthor:wiesel&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
+    .then(function(response) {
+        console.log(response.data)
+        $scope.getBookTitle = response.data.items[0].volumeInfo.title
+        $scope.getBookImg = response.data.items[0].volumeInfo.imageLinks.thumbnail
+     })
+    }
   })
+
+
+// ng-repeat=" bookInfo in getBooks"
