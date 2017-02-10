1008d071364088249b68cc955c88c43a2c6e5d04
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-02-10 10:45:33 -0600

    making dev branch

--- controllers/browse.js
+++ controllers/browse.js
@@ -3,8 +3,9 @@ console.log('browse controller')
 
 
 app.controller('BrowseCtrl', function($http, $scope) {
-  $http.get(`https://www.googleapis.com/books/v1/volumes?q=holes+inauthor:sachar&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
+  $http.get(`https://www.googleapis.com/books/v1/volumes?q=flower+inauthor:keyes&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
   .then(function(response) {
         $scope.getBooks = response.data
+
     })
   })
