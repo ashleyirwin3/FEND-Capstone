a3e26cc9ed4ecd478deeddf65e5bc28fd08b6cda
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-02-10 15:23:59 -0600

    added scope for book description and author

--- controllers/browse.js
+++ controllers/browse.js
@@ -10,6 +10,8 @@ app.controller('BrowseCtrl', function($http, $scope) {
         console.log(response.data)
         $scope.getBookTitle = response.data.items[0].volumeInfo.title
         $scope.getBookImg = response.data.items[0].volumeInfo.imageLinks.thumbnail
+        $scope.getBookDes = response.data.items[0].volumeInfo.description
+        $scope.getBookAuthor = response.data.items[0].volumeInfo.authors
      })
     }
   })
