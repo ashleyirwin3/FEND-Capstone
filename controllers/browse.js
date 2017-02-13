f8c47be001f37b2b2fe36bd6d979b43f6bd857cf
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-02-13 11:42:34 -0600

    mutliple books now populate the DOM

--- controllers/browse.js
+++ controllers/browse.js
@@ -2,19 +2,18 @@ console.log('browse controller')
 
 
 
-app.controller('BrowseCtrl', function($http, $scope) {
-  $scope.getBooks = ()=> {
+app.controller('BrowseCtrl', function($http, $scope, $q) {
+  $scope.getBooks = (title)=> {
         console.log('hey')
-    $http.get(`https://www.googleapis.com/books/v1/volumes?q=night+inauthor:wiesel&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
+    $http.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
     .then(function(response) {
         console.log(response.data)
-        $scope.getBookTitle = response.data.items[0].volumeInfo.title
-        $scope.getBookImg = response.data.items[0].volumeInfo.imageLinks.thumbnail
-        $scope.getBookDes = response.data.items[0].volumeInfo.description
-        $scope.getBookAuthor = response.data.items[0].volumeInfo.authors[0]
+        $scope.data = response.data.items
+        console.log($scope.data)
+        // $scope.getBookTitle = response.data.items[0].volumeInfo.title
+        // $scope.getBookImg = response.data.items[0].volumeInfo.imageLinks.thumbnail
+        // $scope.getBookDes = response.data.items[0].volumeInfo.description
+        // $scope.getBookAuthor = response.data.items[0].volumeInfo.authors[0]
      })
     }
   })
-
-
-// ng-repeat=" bookInfo in getBooks"
