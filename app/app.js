7f200c1ced461f8ac9c8ca0fe367c97d360e71a2
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-02-13 11:42:05 -0600

    began testing function for searching books by title

--- app/app.js
+++ app/app.js
@@ -1,3 +1,9 @@
 console.log('testing')
 
 const app = angular.module('bookShelf', ['ngRoute'])
+
+  function findBooks(books) {
+     $http.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
+      .then(function(response) {
+  })
+}
