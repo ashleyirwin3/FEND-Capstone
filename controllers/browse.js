44e2e1b31e7bd5389c1f4e28c77417f1e3129764
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-02-15 13:35:06 -0600

    user can not search books by title and add books to their profile

--- controllers/browse.js
+++ controllers/browse.js
@@ -2,18 +2,23 @@ console.log('browse controller')
 
 
 
-app.controller('BrowseCtrl', function($http, $scope, $q) {
+app.controller('BrowseCtrl', function($http, $scope) {
   $scope.getBooks = (title)=> {
         console.log('hey')
-    $http.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
+    $http.get(`https://www.googleapis.com/books/v1/volumes?q=${$scope.title}&key=AIzaSyByuWvTo_uYeZIdTq3NTknCjoi3yjQVkAE`)
     .then(function(response) {
         console.log(response.data)
         $scope.data = response.data.items
         console.log($scope.data)
-        // $scope.getBookTitle = response.data.items[0].volumeInfo.title
-        // $scope.getBookImg = response.data.items[0].volumeInfo.imageLinks.thumbnail
-        // $scope.getBookDes = response.data.items[0].volumeInfo.description
-        // $scope.getBookAuthor = response.data.items[0].volumeInfo.authors[0]
+     })
+    }
+    $scope.addBooks = (data)=> {
+        console.log('hey')
+    $http.post(`https://front-end-capstone-290ae.firebaseio.com/.json`, data)
+    .then(function(response) {
+        console.log(response)
+        $scope.data = response.data.items
+        console.log($scope.data)
      })
     }
   })
